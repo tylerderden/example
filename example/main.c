69f119e1b135218ef9f6b6ca425ae4459fb2d657
tyler derden <Mac@jerremy.local>
2014-03-09 22:57:16 +0400

    Initial Commit

---
+++ example/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  example
+//
+//  Created by tyler derden on 09.03.14.
+//  Copyright (c) 2014 tyler. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
